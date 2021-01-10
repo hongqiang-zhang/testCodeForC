63bd7f51d29a92020e970a4aac6cc6bead6bdead
hongqiang <zhq_741@163.com>
2021-01-10 19:01:20 +0800

    changes: 1\add file about testbranchAfile 2\modify the latest commit message

---
+++ testbranchAfile/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    int a = 0;
+    int b = 10;
+
+    printf("%d, %d\n", a, b);
+    scanf("%d%d",&a, &b);
+    printf("%d, %d\n", a, b);
+}
